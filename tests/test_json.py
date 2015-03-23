c16819d94c9a6dba284ebd0fdb2a20c49a32d75b
Serge Matveenko <smatveenko@ptsecurity.com>
2015-03-23 19:12:22 +0300

    Add validator_from_jsons test.

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,6 +1,11 @@
-from cerberus_loaders.json import validator_from_json
+from cerberus_loaders.json import validator_from_json, validator_from_jsons
 
 
 def test_json(json_schema_file, validator):
     v = validator_from_json(json_schema_file)
+    assert v.schema == validator.schema
+
+
+def test_jsons(json_schema_string, validator):
+    v = validator_from_jsons(json_schema_string)
     assert v.schema == validator.schema
